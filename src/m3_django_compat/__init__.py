83467a290b245686a060acd7a1fb9773f444e716
Андрей Чагочкин <andrey.chagochkin@bars-open.ru>
2016-04-20 21:56:32 +0600

    Поддержка работы с моделью учетной записи.
    Начиная с 1.5 в Django появилась возможность создавать свою модель
    учетной записи вместо модели django.contrib.auth.models.User.
    
    Данная доработка доавляет функционал, позволяющий одинаково работать с
    моделью учетной:
    
      - переменная AUTH_USER_MODEL, содержащая строку с названием приложения
        и названием модели через точку;
      - функцию get_user_model(), возвращающую класс модели учетной записи.

--- src/m3_django_compat/__init__.py
+++ src/m3_django_compat/__init__.py
@@ -1,1 +1,43 @@
 # coding: utf-8
+from django import VERSION
+from django.conf import settings
+from django.db.models.loading import get_model
+
+
+_VERSION = VERSION[:2]
+
+
+assert (1, 4) <= _VERSION <= (1, 5)
+
+
+_14 = _VERSION == (1, 4)
+_15 = _VERSION == (1, 5)
+# -----------------------------------------------------------------------------
+# Модель "Учетная запись"
+
+
+#: Содержит имя приложения и имя класса модели учетной записи.
+#:
+#: Строка содержит данные в виде, пригодном для использования при описании
+#: внешних ключей (`ForeignKey``, ``OneToOneField`` и т.п.), ссылающихся
+#: на модель учетной записи.
+#:
+#: Для Django 1.4 всегда содержит значение `'auth.User'`, а для более старших
+#: версий - значение параметра `AUTH_USER_MODEL` из настроек системы.
+#:
+#: .. code::
+#:
+#:    from m3_django_compat import AUTH_USER_MODEL
+#:    class Person(models.Model):
+#:        user = models.ForeignKey(AUTH_USER_MODEL)
+AUTH_USER_MODEL = 'auth.User' if _14 else settings.AUTH_USER_MODEL
+
+
+def get_user_model():
+    u"""Возвращает класс модели учетной записи.
+
+    Для Django 1.4 возвращает :class:`django.contrib.auth.models.User`, а для
+    версий 1.5 и старше - модель, указанную в параметре `AUTH_USER_MODEL`.
+    """
+    return get_model(*AUTH_USER_MODEL.split('.'))
+# -----------------------------------------------------------------------------
