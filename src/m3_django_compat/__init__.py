5c80b4cabfee01eeb1cd2967e85f1458a3f2d2fb
Ivan Panov <i.panov@bars.group>
2019-04-04 12:37:35 +0700

    (EDUCLLG-4444) Добавлена поддержка django 2.2.

--- src/m3_django_compat/__init__.py
+++ src/m3_django_compat/__init__.py
@@ -26,7 +26,7 @@ _14 = _VERSION == (1, 4)
 MIN_SUPPORTED_VERSION = (1, 4)
 
 #: Максимальная поддерживаемая версия Django.
-MAX_SUPPORTED_VERSION = (2, 1)
+MAX_SUPPORTED_VERSION = (2, 2)
 
 assert MIN_SUPPORTED_VERSION <= _VERSION <= MAX_SUPPORTED_VERSION, (
     'Unsupported Django version: {}.{}'.format(*_VERSION)
